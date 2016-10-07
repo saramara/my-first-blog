e4fd6c5bd5c2f4de6716eec9e76737459c21d57e
sara <saretta86m@gmail.com>
2016-10-07 21:12:01 +0200

    import mancante

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from .forms import PostForm
 from django.utils import timezone
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
